de342b52fd4620b54ffba97347757b0a203ddd92
janetahn <jjanetahn@gmail.com>
2026-01-25 21:45:31 -0800

    Initial Commit

---
+++ IncomeApp/IncomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeAppApp.swift
+//  IncomeApp
+//
+//  Created by Janet Ahn on 2026-01-25.
+//
+
+import SwiftUI
+
+@main
+struct IncomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
